a27db8365ea298eaae423a2b54bd2c3321a85af3
Giuseppe <giuseppebrandalise@gmail.com>
2023-01-16 02:53:38 -0300

    Requisitos 5 e 6

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
 
